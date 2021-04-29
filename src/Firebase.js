73200408480c6848c6a2f6b25f7c30f7a296c987
Samarth Nehe <samarthnehe2000@gmail.com>
2021-04-29 19:09:33 +0530

    done

---
+++ src/Firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAxIub7JJ604UiCPGOOkO7WyriiokcCArk",
+    authDomain: "mysite-2e4fe.firebaseapp.com",
+    projectId: "mysite-2e4fe",
+    storageBucket: "mysite-2e4fe.appspot.com",
+    messagingSenderId: "694047067753",
+    appId: "1:694047067753:web:ec3d0adebe073f719a9cf8",
+    measurementId: "G-0SMPWHHWXG"
+  };
